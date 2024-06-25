bdd12a307122fff9cec7462293a3a0783e6826b1
aye, kyi lei <kaye2@go.pasadena.edu>
2024-06-25 02:28:59 -0700

    Training and evaluating the model

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 #Loading Dataset
 col_names = ['ID','Diagnosis','radius1','texture1','perimeter1','area1','smoothness1','compactness1','concavity1','concave_points1','symmetry1','fractal_dimension1',
@@ -19,15 +20,41 @@ data = pd.read_csv("wdbc.data",header=None, names=col_names)
 #print()
 
 #Converting target char value to numerical value 
+#Mapping 'B' to 0 and 'M' to 1
 data['Diagnosis'] = data['Diagnosis'].map({'B':0, 'M': 1})
 
 #Splitting dataset into features and target variables
-x = data.drop(['ID','Diagnosis'],axis=1) #30 features
+X = data.drop(['ID','Diagnosis'],axis=1) #30 features
 y = data['Diagnosis'] # target
 #print(x.columns)
 #print(y)
 
-#Visualizing dataset with scatterplot
-#plt.scatter(x,y)
-#plt.title("Scatter Plot of Breast Cancer Prediction Logistic Regression")
-#plt.show()
+#Splitting data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+#Feature Scaling to ensure that all features equally contribute to target column
+scaler = StandardScaler() 
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+#Create and train the logistic regression model
+logreg = LogisticRegression()
+logreg.fit(X_train_scaled, y_train)
+
+#Predictions
+y_pred = logreg.predict(X_test_scaled)
+
+print("==== Actual vs Predicted Result ====")
+for i in range(len(y_pred)):
+    print(f"Actual Result: {y_test.iloc[i]}  Predicted Result: {y_pred[i]}")
+print()
+
+#Evaluate the model
+cnf = confusion_matrix(y_pred, y_test)
+print("Confusion Matrix: ")
+print(f"{cnf}")
+
+accuracy = accuracy_score(y_test, y_pred)
+accuracy_percent = accuracy * 100
+print(f"Accuracy of the model: {accuracy_percent:.2f}%")
+
